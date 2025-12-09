84ed27e68463a3e0396609b2342aac03548c1ac0
yomna-ahmed274 <yomna.a.farag@gmail.com>
2025-12-10 01:29:26 +0200

    Add XML compression and decompression functionality

--- main.cpp
+++ main.cpp
@@ -27,5 +27,53 @@ int main() {
         cout << "\n--- Fixed XML ---\n";
         cout << fixedXML;
     }
+
+    // Compress and Decompress main code ==>> Yomna
+    int choice;
+
+    cout << "=== XML File Compression & Decompression Tool ===\n";
+    cout << "1. Compress an XML file\n";
+    cout << "2. Decompress a compressed file\n";
+    cout << "3. Exit\n";
+    cout << "Enter your choice (1-3): ";
+    cin >> choice;
+    cin.ignore();  // Clear newline
+
+    switch (choice) {
+    case 1: {
+        SimpleXMLCompressor compressor;
+        string inputFile, outputFile;
+
+        cout << "Enter input XML filename: ";
+        getline(cin, inputFile);
+
+        cout << "Enter output compressed filename: ";
+        getline(cin, outputFile);
+
+        compressor.compress(inputFile, outputFile);
+        break;
+    }
+    case 2: {
+        SimpleXMLDecompressor decompressor;
+
+        string inputFile, outputFile;
+
+        cout << "Enter input compressed filename: ";
+        getline(cin, inputFile);
+
+        cout << "Enter output XML filename: ";
+        getline(cin, outputFile);
+
+        decompressor.decompress(inputFile, outputFile);
+        break;
+    }
+    case 3:
+        cout << "Goodbye!\n";
+        break;
+
+    default:
+        cout << "Invalid choice!\n";
+    }
+    
     return 0;
 }
