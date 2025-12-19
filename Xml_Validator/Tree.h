4e1915b1511b53e9773019486129ac9b22c79b06
Salmaramadan348 <salma.ramadan.mohammed@gmail.com>
2025-12-19 23:01:43 +0200

    Add Tree class header file

---
+++ Xml_Validator/Tree.h
@@ -0,0 +1,15 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "TreeNode.h"
+
+class Tree {
+private:
+    TreeNode* root;
+
+public:
+    Tree();                
+    TreeNode* getRoot();
+};
+
+#endif
