9481121be3a25b61bb303b10228dad6379b6cab7
AyaMohamed234 <aya666777888aya@gmail.com>
2025-12-21 22:53:46 +0200

    Update minify.h

--- Minify/minify.h
+++ Minify/minify.h
@@ -1,9 +1,11 @@
 #ifndef MINIFY_XML_H
 #define MINIFY_XML_H
-
 #include <string>
+#include "Tree.h"
 
 std::string trim(const std::string& s);
 std::string minifyXML(const std::string& xml);
+Tree* parseXMLToTree(const std::string& xml);
+std::string treeToMinifiedXML(TreeNode* node);
 
 #endif
