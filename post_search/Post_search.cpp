241e6135d8d5d9f15d0db10c95a09202984066d1
Salmaramadan348 <salma.ramadan.mohammed@gmail.com>
2025-12-22 22:23:29 +0200

    Update Post_search

--- post_search/Post_search.cpp
+++ post_search/Post_search.cpp
@@ -1,4 +1,4 @@
-﻿#include "Post_search.h"
+#include "Post_search.h"
 #include "XmlParser.h"
 
 std::vector<PostResult> searchByTopicWithUser(const std::vector<User>& users, const std::string& key) {
@@ -7,7 +7,7 @@ std::vector<PostResult> searchByTopicWithUser(const std::vector<User>& users, co
     for (const User& user : users) {
         for (const Post& post : user.posts) {
             for (const std::string& topic : post.topics) {
-                if (parser.trim(topic) == key) {
+                if (XmlParser::trim(topic) == key) {
                     results.push_back({ user.id, post });
                     break;
                 }
@@ -23,7 +23,7 @@ std::vector<PostResult> searchByWordWithUser(const std::vector<User>& users, con
     XmlParser parser;
     for (const User& user : users) {
         for (const Post& post : user.posts) {
-            std::string body = parser.trim(post.body);
+            std::string body = XmlParser::trim(post.body);
             if (body.find(word) != std::string::npos) {
                 results.push_back({ user.id, post });
             }
